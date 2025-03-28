964fcc9a75c20d9b3fce8e545e01e60138fac6d3
Yaroslav Kepko <yaroslavkepko@gmail.com>
2025-03-28 19:12:01 +0100

    New commit: 	Add new function gcd() to find greatest common divisor which will be used to determine amount of cycles in the array and amount of shiftings in each cycle

--- main.c
+++ main.c
@@ -1,17 +1,43 @@
 #include <stdio.h>
 
-	void cyklicky_posun_rychlo (int *p, int l, int s);
+	void cyklicky_posun_rychlo(int *p, int l, int s);
+	int gcd(int array_size, int shift_size);
 
-	int main (void)
+	int main(void)
 		{
 			int array[6] = {5, 4, 2, 7, 8, 9};
-			int array_size = sizeof(array) / sizeof(array[0]);
+			int array_size = sizeof(array) / sizeof(array[0]); 
 			
-			cyklicky_posun_rychlo(array, array_size, 5);
+			printf("%d\n",gcd(11,4));
+			// cyklicky_posun_rychlo(array, array_size, 5);
 			return 0;
 		}
-
-	void cyklicky_posun_rychlo (int *p, int array_size, int shift_size)
+ 
+	int gcd(int array_size, int shift_size)
 		{
+			if(array_size == 0)
+				return shift_size;
 
+			if(shift_size == 0)
+				return array_size;
+
+			if (array_size == shift_size)
+				return array_size;
+
+			if(array_size > shift_size)
+				return gcd(array_size - shift_size, shift_size);
+
+			if(shift_size > array_size)
+				return gcd(array_size, shift_size - array_size);
 		}
+
+	void cyklicky_posun_rychlo(int *p, int array_size, int shift_size)
+		{
+			int *temp;
+
+			shift_size %= array_size; //Reduce shift size
+
+			if(shift_size == 0) 
+				return;
+		}
+
