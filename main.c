57e7ca23b7ac91b71d88b9eae93f24fbb8ae99bb
Yaroslav Kepko <yaroslavkepko@gmail.com>
2025-03-28 21:42:49 +0100

    New changes in function cyklicky_posun_rychlo: Function moves elements of the array to the right in case when gcd of array size and number of shifts equals 1 (means that array contains only one cycle)

--- main.c
+++ main.c
@@ -5,11 +5,18 @@
 
 	int main(void)
 		{
-			int array[6] = {5, 4, 2, 7, 8, 9};
-			int array_size = sizeof(array) / sizeof(array[0]); 
-			
-			printf("%d\n",gcd(11,4));
-			// cyklicky_posun_rychlo(array, array_size, 5);
+			int array[7] = {5, 4, 2, 7, 8, 9, 6};
+			int array_size = sizeof(array) / sizeof(array[0]);
+
+			for(int i = 0; i < array_size; i++)
+				printf("%d ", array[i]);
+				puts("");
+			// printf("%d\n",gcd(11,4));
+			cyklicky_posun_rychlo(array, array_size, 5);
+
+			for(int i = 0; i < array_size; i++)
+				printf("%d ", array[i]);
+
 			return 0;
 		}
  
@@ -33,11 +40,39 @@
 
 	void cyklicky_posun_rychlo(int *p, int array_size, int shift_size)
 		{
-			int *temp;
+			int temp;
+			int current_value;
+			int gcd1;
+			int* p1 = p;
+			int counter = 0;
+			int index = 0;
+			int current_index = 0;
+			int next = 0;
 
 			shift_size %= array_size; //Reduce shift size
 
 			if(shift_size == 0) 
-				return;
+				{
+					printf("Number of shifts equals the array size so no moves are needed\n");
+					return;
+				}
+
+			gcd1 = gcd(array_size, shift_size);
+
+			current_value = *p;
+
+			if(gcd1 == 1)
+				{
+					do
+						{
+							current_index += shift_size;
+							current_index %= array_size;
+							temp = p[current_index]; 
+							p[current_index] = current_value;
+							current_value = temp;
+							counter++;
+
+						} while(counter != array_size);
+				}
 		}
 
